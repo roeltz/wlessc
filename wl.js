8bdfe19e6ff1746905b4f38565eb6154b2bed378
roeltz <leo_rothe@msn.com>
2014-01-05 19:19:08 -0430

    Look for @import directives and watch them too

--- wl.js
+++ wl.js
@@ -3,14 +3,40 @@
 var fs = require("fs");
 var path = require("path");
 var less = require("less");
+var traverse = require("traverse");
 
-function compile(file, callback) {
-
+function imports(file, callback) {
+	var importedFilenames = [];
 	var parser = new less.Parser({
         paths: [path.dirname(file)],
         filename: input
     });
-    
+	fs.readFile(file, "utf-8", function(err, data){
+		if (err) {
+			callback(err);
+		} else {
+			parser.parse(data, function (err, tree) {
+				if (err) {
+					callback(err);
+				} else {
+					traverse(tree).forEach(function(x){
+						if (x instanceof less.tree.Import) {
+							importedFilenames.push(x.importedFilename);
+						}
+					});
+					callback(null, importedFilenames);
+				}
+			});
+		}
+	});	
+}
+
+function compile(file, callback) {
+	var parser = new less.Parser({
+        paths: [path.dirname(file)],
+        filename: input
+    });
+
     fs.readFile(file, "utf-8", function(err, data){
     	if (err) {
     		callback(err);
@@ -31,36 +57,52 @@ function compile(file, callback) {
     });
 }
 
+function watch(file, callback) {
+	fs.watch(file, {persistent: true}, (function(){
+		var tid;
+		return function() {
+			clearTimeout(tid);
+			tid = setTimeout(callback, 500);
+		};
+	})());
+}
+
 var input = path.resolve(process.cwd(), process.argv[2] || "style.less");
 
 if (fs.existsSync(input)) {
 	var output = path.join(path.dirname(input), path.basename(input, ".less") + ".css");
 
 	function onchange(){
-	    console.log("wl: compiling", input);
+	    console.log("Compiling...");
 	    compile(input, function(err, css){
 	    	if (err) {
-	    		if (err.type === "Parse")
+	    		if (err.type === "Parse") {
 	    			err = less.formatError(err);
-	    		console.error("wl: error:", ("\n" + err).split(/\n/gm).join("\n\t"));
-	    		process.exit(1);
+	    			console.error("Error:", ("\n" + err).split(/\n/gm).join("\n\t"));
+	    		} else {
+	    			console.error(err);
+	    		}
 	    	} else {
-		    	fs.writeSync(output, css);
-		    	console.log("wl: done");
+		    	fs.writeFileSync(output, css);
+		    	console.log("Done.");
 	    	}
     	});
     }
-
-	fs.watch(input, {persistent: true}, (function(){
-		var tid;
-		return function() {
-			clearTimeout(tid);
-			tid = setTimeout(onchange, 500);
-		};
-	})());
-	
-	console.log("Watching ", path.relative(process.cwd(), input), ">", path.relative(process.cwd(), output));
+    
+    imports(input, function(err, paths){
+    	if (err) {
+    		console.error("Error while scanning for @import rules:", err);
+    	} else {
+	    	paths.unshift(input);
+	    	console.log("Watching:");
+    		paths.forEach(function(file){
+		    	console.log("\t" + file);
+    			watch(file, onchange);
+    		});
+	    	console.log("Output:\n\t", output, "\n");
+    		onchange();
+	    }
+    });
 } else {
 	console.error("File does not exist:", input);
-	process.exit(1);
 }
